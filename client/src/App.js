9a6a29dc57a22174c498b7746830ac0efa325f2c
Tyson McDonald <tyson22509@hotmail.com>
2021-02-08 10:49:47 -0500

    installed semantic ui and created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Menubar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>OmadaText</h1>
-    </div>
+    <Router>
+      <Container>
+        <Menubar/>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+      </Container>
+    </Router>
   );
 }
 
